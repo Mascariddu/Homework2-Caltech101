40ab055d7777ad71198325414d20642ec4f60c50
Andrea Mascarello <48280975+Mascariddu@users.noreply.github.com>
2020-04-29 19:30:04 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -16,6 +16,7 @@ def pil_loader(path):
         return img.convert('RGB')
     
 
+
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
